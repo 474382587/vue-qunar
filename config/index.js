26a9c229442bf232d62139001ee7dfc5ddf49448
Joseph Jin <jerkjoe61@gmail.com>
2018-06-10 23:18:00 -0700

    path change

--- config/index.js
+++ config/index.js
@@ -10,7 +10,14 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/dist': {
+        'target': 'http://localhost:8080',
+        'pathRewrite': {
+          '^/dist': '/'
+        }
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
