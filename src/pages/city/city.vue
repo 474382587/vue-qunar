69dd75e98f1f31968379c797751f1fe4ecdd5c23
Joseph Jin <jerkjoe61@gmail.com>
2018-06-10 18:11:41 -0700

    search bar style of city page

--- src/pages/city/city.vue
+++ src/pages/city/city.vue
@@ -1,15 +1,18 @@
 <template>
     <div>
         <city-header></city-header>
+        <city-search></city-search>
     </div>
 </template>
 
 <script>
 import CityHeader from './components/header.vue'
+import CitySearch from './components/search.vue'
 export default {
   name: 'City',
   components: {
-    'city-header': CityHeader
+    'city-header': CityHeader,
+    'city-search': CitySearch
   }
 }
 </script>
---
+++ src/pages/city/components/search.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="search">
+        <input type="text" placeholder="Please enter city name..." class="search-input">
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/variables.styl';
+.search
+    height .72rem
+    background-color $bgColor
+    padding 0 .1rem
+    .search-input
+        box-sizing border-box
+        width 100%
+        height .62rem
+        padding 0 .1rem
+        line-height .62rem
+        text-align center
+        border-radius .06rem
+        color #666
+</style>
