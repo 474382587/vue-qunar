5948ac488193d05cae37c1b47bada9261c7240d9
Joseph Jin <jerkjoe61@gmail.com>
2018-06-11 22:49:16 -0700

    search logic and pop up result 1) also made some logic so that user is able to tell search is finished

--- src/pages/city/components/search.vue
+++ src/pages/city/components/search.vue
@@ -1,12 +1,65 @@
 <template>
-    <div class="search">
-        <input type="text" placeholder="Please enter city name..." class="search-input">
+    <div>
+        <div class="search">
+            <input v-model="keyword" type="text" placeholder="Please enter city name..." class="search-input">
+        </div>
+        <div class="search-content" ref="search" v-show="keyword">
+            <ul>
+                <li class="search-item border-bottom" v-for="item in list" :key="item.id">
+                    {{item.spell.charAt(0).toUpperCase() + item.spell.slice(1)}}
+                </li>
+                <li class="search-item border-bottom" v-if="hasNoData">Result not found.</li>
+            </ul>
+        </div>
     </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timmer: null,
+      enterData: false
+    }
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length && !this.enterData
+    }
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs['search'])
+  },
+  watch: {
+    keyword () {
+      this.enterData = true
+      if (this.timmer) {
+        clearTimeout(this.timmer)
+      }
+      this.timmer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach(value => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+        if (!this.keyword) {
+          this.list = []
+        }
+        this.enterData = false
+      }, 100)
+    }
+  }
 }
 </script>
 
@@ -25,4 +78,21 @@ export default {
         text-align center
         border-radius .06rem
         color #666
+.search-content
+    z-index 1
+    overflow hidden
+    position absolute
+    top 1.58rem
+    left 0
+    right 0
+    bottom 0
+    background-color #eee
+    .search-item
+        line-height .62rem
+        padding-left .2rem
+        color #666
+        background-color #fff
+.border-bottom
+    &:before
+        background-color #e3e3e3
 </style>
