8f167fd998bcbc5f06494c4ef4fd331d6ea226db
Joseph Jin <jinyuhui61@hotmail.com>
2018-06-13 16:25:18 -0700

    detail-gallery implemented

---
+++ src/common/gallery/gallery.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="container" @click="closeImageGallery">
+    <div class="wrapper">
+      <swiper :options="swiperOptions">
+        <!-- slides -->
+        <swiper-slide v-for="img in imageList" :key="img.id">
+            <img class="swiper-img" :src="img.imgUrl" :alt="img.id" >
+        </swiper-slide>
+        <!-- Optional controls -->
+        <div class="swiper-pagination" slot="pagination"></div>
+      </swiper>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CommonGallery',
+  props: {
+    imageList: Array
+  },
+  data () {
+    return {
+      swiperOptions: {
+        pagination: '.swiper-pagination',
+        paginationType: 'fraction',
+        observeParents: true,
+        observer: true
+      }
+    }
+  },
+  methods: {
+    closeImageGallery () {
+      this.$emit('hide')
+    }
+  },
+  mounted () {
+    console.log(this.imageList)
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.container >>> .swiper-container
+    overflow: inherit
+  .container
+    display: flex
+    flex-direction: column
+    justify-content: center
+    z-index: 99
+    position: fixed
+    left: 0
+    right: 0
+    top: 0
+    bottom: 0
+    background: #000
+    .wrapper
+      height: 0
+      width: 100%
+      padding-bottom: 100%
+      .swiper-img
+        width: 100%
+      .swiper-pagination
+        color: #fff
+        bottom: -1rem
+</style>
