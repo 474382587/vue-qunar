047d4408a6f27427a1626384ab02f4d0bf302cbe
Joseph Jin <jerkjoe61@gmail.com>
2018-06-14 19:20:36 -0700

    detail list page recursive child component

--- src/pages/detail/detail.vue
+++ src/pages/detail/detail.vue
@@ -2,37 +2,74 @@
     <div>
       <detail-banner :imageList="imageList"></detail-banner>
       <detail-header :imageList="imageList"></detail-header>
-      <div class="content">Dummy Data</div>
+      <div class="content">
+        <detail-list :list="list"></detail-list>
+      </div>
     </div>
 </template>
 
 <script>
 import Banner from './components/banner.vue'
 import DetailHeader from './components/header.vue'
+import DetailList from './components/list.vue'
 import axios from 'axios'
 export default {
   name: 'Detail',
   data () {
     return {
-      imageList: []
+      imageList: [],
+      list: [
+        {
+          title: 'Adult',
+          children: [
+            {
+              title: 'Adult Ticket Rate1',
+              children: [
+                {
+                  title: 'Adult Ticket Rate11'
+                },
+                {
+                  title: 'Adult Ticket Rate12'
+                }
+              ]
+            },
+            {
+              title: 'Adult Ticket Rate2'
+            },
+            {
+              title: 'Adult Ticket Rate3'
+            }
+          ]
+        },
+        {
+          title: 'Child'
+        },
+        {
+          title: 'Senior'
+        },
+        {
+          title: 'Special'
+        }
+      ]
     }
   },
   components: {
     'detail-banner': Banner,
-    'detail-header': DetailHeader
+    'detail-header': DetailHeader,
+    'detail-list': DetailList
   },
   mounted () {
-    axios.get('/vue-qunar/dist/static/mock/gallery.json')
-      .then(res => {
-        res = res.data
-        const data = res.data
-        this.imageList = data.galleryImage
-      })
+    axios.get('/vue-qunar/dist/static/mock/gallery.json').then(res => {
+      res = res.data
+      const data = res.data
+      this.imageList = data.galleryImage
+    })
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-.content
-  height 50rem
+.content {
+  height: 50rem;
+}
 </style>
